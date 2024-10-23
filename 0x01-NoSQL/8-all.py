ff4b9431764f40230b589613b2de0ec6e259d415
yadsashel <ysalhi600@gmail.com>
2024-10-23 08:29:48 -0700

    fixing task 8 bugs

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-
-"""
-a Python function that lists all documents in a collection
-"""
+'''Task 8's module.
+'''
 
 
 def list_all(mongo_collection):
+    '''Lists all documents in a collection.
+    '''
     return [doc for doc in mongo_collection.find()]
